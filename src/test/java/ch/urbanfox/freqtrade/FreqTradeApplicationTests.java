c4e7b334a724bb83ac824eec3eb95b6b2e959537
ChrisTimperley <christimperley@googlemail.com>
2020-11-23 17:02:52 -0500

    Added skeleton for FreqTradeMainRunnerTest

--- src/test/java/ch/urbanfox/freqtrade/FreqTradeApplicationTests.java
+++ src/test/java/ch/urbanfox/freqtrade/FreqTradeApplicationTests.java
@@ -2,17 +2,23 @@ package ch.urbanfox.freqtrade;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @ActiveProfiles(profiles = "test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FreqTradeApplicationTests {
+    @Autowired
+    private FreqTradeMainRunner mainRunner;
 
     @Test
     public void contextLoads() {
+        assertEquals(false, mainRunner.getState());
     }
 
 }
